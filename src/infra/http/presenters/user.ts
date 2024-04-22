3dfd7a94aaeba9857f84490f9a893e3dcad204e6
Pedro Mendes <pedrohenriquecamposmendes@hotmail.com>
2024-04-22 17:53:50 -0300

    fix: userpresenter

--- src/infra/http/presenters/user.ts
+++ src/infra/http/presenters/user.ts
@@ -3,7 +3,7 @@ import { User } from '@/domain/food-ordering/enterprise/entities/user'
 export class UserPresenter {
   static toHttpResponse(user: User) {
     return {
-      id: user.id,
+      id: user.id.toString(),
       name: user.name,
       email: user.email,
       role: user.role,
