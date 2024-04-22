e94a53f4201c382c99749e1b73cf721cdf60cf4b
Pedro Mendes <pedrohenriquecamposmendes@hotmail.com>
2024-04-22 17:17:51 -0300

    chore: setup fastify config

---
+++ src/infra/http/server.ts
@@ -0,0 +1,41 @@
+import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
+
+import { errorHandler } from './error-handler'
+
+const app = fastify()
+
+// ! Fastify TypeProviderZod config to type routes inputs and outputs
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+// ! Fastify Swagger config
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    info: {
+      title: 'food-ordering',
+      description: 'food-ordering API documentation',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
+
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+app.setErrorHandler(errorHandler)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('[server] HTTP server listening on port 3333')
+})
